f00fd7bde3043bdfe28f5063a8e79c7b1ab84755
Emielio <e.hoogeboom@student.tudelft.nl>
2014-11-06 15:20:16 +0100

    Initial Commit

---
+++ Hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hangman
+//
+//  Created by Emiel on 11/6/14.
+//  Copyright (c) 2014 mprog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
